f987986568fd8669d3c2321e03484a67d18f2505
Nikita Epifantsev <eppy_92@mail.ru>
2024-07-18 00:28:43 +0300

    Value Obj. class added

--- Access_control_system/Models/Models.cs
+++ Access_control_system/Models/Models.cs
@@ -1,31 +1,29 @@
 ﻿using Access_control_system.Extensions;
+using Access_control_system.Models;
+using System;
 using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
+using System.Text.RegularExpressions;
 
 namespace Access_control_system.Models
 {
     public class User
     {
-        public User(string emailAddress, string password, string department, string position, string name,
-            string phoneNumber, string city, DateTime sessionType)
+        public User(string email, string password, string name,
+            PhoneNumber phoneNumber)
         {
-            Login = emailAddress;
+            Email = email;
             Password = password;
-            Department = department;
-            Position = position;
             Name = name;
             PhoneNumber = phoneNumber;
-            City = city;
-            SessionTime = sessionType.; /* Необходимо получить дату выхода из системы и через TimeSpan определить 
-                                         длительность сессии */
-            Id = Login.GetDeterministicHashCode();
+            Id = Email.GetDeterministicHashCode();
         }
 
-        [Required(ErrorMessage = "Login required")]
+        [Required(ErrorMessage = "Email required")]
         [RegularExpression(@"^[\w!#$%&'*+\-/=?\^_`{|}~]+(\.[\w!#$%&'*+\-/=?\^_`{|}~]+)*" + "@"
             + @"((([\-\w]+\.)+[a-zA-Z]{2,4})|(([0-9]{1,3}\.){3}[0-9]{1,3}))$", ErrorMessage = "Not a valid email")]
         [DisplayName("Login")]
-        public string Login { get; set; }
+        public string Email { get; set; }
 
         [Required(ErrorMessage = "Password required")]
         [StringLength(10, MinimumLength = 3, ErrorMessage = "Wrong password. Please, try again.")]
@@ -34,23 +32,49 @@ namespace Access_control_system.Models
 
         [Required(ErrorMessage = "Password required")]
         [Compare("Password", ErrorMessage = "Passwords don't match")]
-        public string ConfirmPassword { get; set; } 
+        public string ConfirmPassword { get; set; }
 
         [Required]
         public string Name { get; set; }
 
         [Required]
         [Range(16, 120, ErrorMessage = "Age must be between 16 and 120")]
-        public string Age { get; set; }
+        public Age Age { get; set; }
 
-        [RegularExpression(@"^\+[1-9]\d{3}-\d{3}-\d{4}$")]
-        public string PhoneNumber { get; set; }
+        [Required]
+        public PhoneNumber PhoneNumber { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime LoggedinAt { get; set; }
 
-        [DataType(DataType.Time)] // т.к. будем высчитывать кол-во часов пользователя в системе
-        public DateTime SessionTime { get; set; }
-        public string Department { get; set; }
-        public string Position { get; set; }
-        public string City { get; set; }
         private long Id { get; set; }
+    }
+}
+
+
+public class PhoneNumber : ValueObject
+{
+    private const string phoneRegex = @"^\(?([0-9]{3})\)?[-. ]?([0-9]{3})[-. ]?([0-9]{4})$"; // TODO:добавить валидацию
+    public string Number { get; }
+    public PhoneNumber(string number)
+    {
+        Number = number;
+    }
+    protected override IEnumerable<object> GetEqualityComponents()
+    {
+        yield return Number;
+    }
+}
+
+
+public class Age : ValueObject
+{
+    public string UserAge { get; } // TODO:добавить валидацию
+    public Age(string userAge)
+    {
+        UserAge = userAge;
+    }
+    protected override IEnumerable<object> GetEqualityComponents()
+    {
+        yield return UserAge;
     }
 }
